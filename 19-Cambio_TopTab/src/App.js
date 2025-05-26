422f37cc7f48487c27fc72f49a46a0c583483ee2
edu9988 <edu.jerm@gmail.com>
2025-05-26 19:06:42 -0300

    Do exercise 19

---
+++ 19-Cambio_TopTab/src/App.js
@@ -0,0 +1,50 @@
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { Ionicons, FontAwesome5, FontAwesome6 } from '@expo/vector-icons'
+import { View } from 'react-native'
+
+import Header from './components/Header'
+import CurrencyConverter from './screens/CurrencyConverter'
+
+const Tab = createMaterialTopTabNavigator()
+
+export default () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={{flex: 1}}>
+      <Header />
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="USD"
+          screenOptions={ ({ route }) => ({
+            headerShown: false,
+            tabBarStyle: {
+              height: 65
+            },
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+              padding: 0,
+            },
+            tabBarActiveTintColor: '#6200ee',
+            tabBarInactiveTintColor: 'gray',
+            tabBarIcon: ({ color }) => (
+              <View style={{
+                width: 60,
+                height: 60,
+                marginVertical: 'auto',
+              }}>
+                { route.name === 'USD' && <Ionicons name="logo-usd" size={50} color={color}/>}
+                { route.name === 'EUR' && <FontAwesome5 name="euro-sign" size={50} color={color}/>}
+                { route.name === 'BTC' && <FontAwesome6 name="bitcoin" size={50} color={color}/>}
+              </View>
+            )
+          })}
+        >
+          <Tab.Screen name="USD" component={CurrencyConverter} initialParams={{ to: "USD" }}/>
+          <Tab.Screen name="EUR" component={CurrencyConverter} initialParams={{ to: "EUR" }}/>
+          <Tab.Screen name="BTC" component={CurrencyConverter} initialParams={{ to: "BTC" }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  </SafeAreaProvider>
+)
---
+++ 19-Cambio_TopTab/src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  button: {
+    width: 280,
+    height: 50,
+    padding: 0,
+    marginHorizontal: 'auto',
+    marginTop: 30,
+    marginBottom: 40,
+    backgroundColor: 'palegreen',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'darkgreen'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 28,
+    color: 'darkgreen'
+  }
+})
